a5fa52012a1e637b3c75a5c080eae31b2a82dc0b
Jainish1808 <135621871+Jainish1808@users.noreply.github.com>
2024-12-11 11:59:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import HomePage from "./components/home/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetail from "./components/itemDetail/ItemDetail";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Cart from "./components/cart/Cart";
+import Orders from "./components/orders/Orders";
+import Checkout from "./components/checkout/Checkout";
+import Bed from "./components/bed/Bed";
+import Chair from "./components/chair/Chair";
+import Curtain from "./components/curtain/Curtain";
+import Sofa from "./components/sofa/Sofa";
+import Table from "./components/table/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/item/:id" element={<ItemDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/bed" element={<Bed />} />
+          <Route path="/chair" element={<Chair />} />
+          <Route path="/curtain" element={<Curtain />} />
+          <Route path="/sofa" element={<Sofa />} />
+          <Route path="/table" element={<Table />} />
+        </Routes>
+        <div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
